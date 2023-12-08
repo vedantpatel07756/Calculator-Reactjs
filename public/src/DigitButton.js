08211ef970b4959f72507ce17d0aa8bf8166ea8c
Vedant Patel <vedantpatel1827@gmail.com>
2023-12-08 20:35:56 +0530

    Delete public directory

--- public/src/DigitButton.js
+++
@@ -1,5 +0,0 @@
-import { ACTIONS } from "./App"
-
-export  function DigitButton({dispatch,digit}){
-    return     <button className="num" onClick={()=> dispatch({type:ACTIONS.ADD_DIGIT,payload:{digit}})}>{digit}</button>
-}
--- public/src/OperationButton.js
+++
@@ -1,5 +0,0 @@
-import { ACTIONS } from "./App"
-
-export function OperationButton({dispatch,operation}){
-    return <button className="op" onClick={ ()=>dispatch({type:ACTIONS.CHOOSE_OPERATION,payload:{operation}})}>{operation}</button>
-}
